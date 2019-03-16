90cd60a7db5e0b7cafc220c0aae1741f1794ef29
Fabien Franzen <info@atelierfabien.be>
2019-03-16 11:49:56 +0100

    Fix MongoDB Native v.3.1.x compatibility

--- test/collection/node.js
+++ test/collection/node.js
@@ -2,12 +2,12 @@
 var assert = require('assert');
 var mongo = require('mongodb');
 
-var uri = process.env.MQUERY_URI || 'mongodb://localhost/mquery';
+var uri = process.env.MQUERY_URI || 'mongodb://localhost:27017/mquery';
 var client;
 var db;
 
 exports.getCollection = function(cb) {
-  mongo.MongoClient.connect(uri, function(err, _client) {
+  mongo.MongoClient.connect(uri, { useNewUrlParser: true }, function(err, _client) {
     assert.ifError(err);
     client = _client;
     db = client.db();
